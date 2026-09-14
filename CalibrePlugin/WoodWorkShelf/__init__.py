e5f7169f369c97040409d4696b39547dbfb99349
josephruocco <ruoccoj19@gmail.com>
2026-09-13 19:53:19 -0600

    Add the WoodWork Calibre shelf plugin

---
+++ CalibrePlugin/WoodWorkShelf/__init__.py
@@ -0,0 +1,12 @@
+from calibre.customize import InterfaceActionBase
+
+
+class WoodWorkShelfPlugin(InterfaceActionBase):
+    name = 'WoodWork Shelf'
+    description = 'Reshuffle the current calibre view into a rotating shelf of books.'
+    supported_platforms = ['windows', 'osx', 'linux']
+    author = 'Joseph Ruocco'
+    version = (1, 0, 0)
+    minimum_calibre_version = (9, 0, 0)
+    actual_plugin = 'calibre_plugins.woodwork_shelf.ui:WoodWorkShelfAction'
+
